b09927bcf884c9905094260ab756220073f8f111
Patryk-Kwasnik <kwasnikzse@interia.pl>
2022-06-03 18:30:31 +0200

    ikony/synchro

---
+++ resources/views/info.blade.php
@@ -0,0 +1,22 @@
+<div class= "info-content">
+    <div class="info-box">
+        <i class="fa-solid fa-person-walking"></i>
+        <h3>Przygody</h3>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    </div>
+    <div class="info-box">
+        <i class="fa-solid fa-globe"></i>
+        <h3>Podróże na cały świat</h3>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    </div>
+    <div class="info-box">
+        <i class="fa-solid fa-circle-dollar-to-slot"></i>
+        <h3>Konkurencyjne ceny</h3>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    </div>
+    <div class="info-box">
+        <i class="fa-solid fa-headphones"></i>
+        <h3>Pomoc 24/7</h3>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    </div>
+</div>
